8fd0d3d17390d45eb0de9ca3772fbe2d9730aa58
parmeshmethre31 <114494186+parmeshmethre31@users.noreply.github.com>
2022-11-16 18:47:47 +0530

    Update index.js

--- sort-visualizer/src/components/molecules/Footer/index.js
+++ sort-visualizer/src/components/molecules/Footer/index.js
@@ -2,41 +2,35 @@ import React from 'react';
 import './style.css';
 
 const Footer = (props) => {
-  return (
-    <footer className="Footer">
-      <section>
-        Designed and built with{' '}
-        <span className="Footer__Heart">&hearts;</span> by{' '}
-        <a
-          href="https://www.linkedin.com/in/chandramogali-gomasi-04888120a/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__Link"
-        >
-          Chandramogali Gomasi
+    return ( 
+      <footer className = "Footer" >
+        <section >
+        Designed and built with { ' ' }
+        <span className = "Footer__Heart" > & hearts; </span> by{' '} 
+        <a href = "https://www.linkedin.com/in/parmesh-methre-ba8b88203/"
+        target = "_blank"
+        rel = "noopener noreferrer"
+        className = "Footer__Link" >
+        Parmesh 
         </a>
-      </section>
+        </section>
 
-      <section className="Footer__Items">
-        {/* <a
-          href="https://youtu.be/JFjvVmvC3pQ"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__Link"
-        >
-          Demo
-        </a> */}
-        <a
-          href="https://github.com/chandramogali/sort-visualizer"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="Footer__Link"
-        >
-          Code
-        </a>
-      </section>
-    </footer>
-  );
+        <section className = "Footer__Items" > {
+            /* <a
+                      href="https://youtu.be/JFjvVmvC3pQ"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="Footer__Link"
+                    >
+                      Demo
+                    </a> */
+        } <
+        a href = "https://github.com/parmeshmethre31"
+        target = "_blank"
+        rel = "noopener noreferrer"
+        className = "Footer__Link" >
+        Code </a> </section> </footer>
+    );
 };
 
 export default Footer;
